2f0b9077520486154f107fc92e5eceaf2780c01b
Kristjan Kirpu <hello@pepzwee.com>
2023-10-16 13:53:49 +0300

    feat: add `axiosOptions` param

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const { JSDOM } = jsdom
 // some mediafire links are instant downloads
 // and that will potentially crash our JSDOM parsing or make node run out of memory
 // so this function makes sure that the given link returns "text/html" content-type
-async function checkLinkResponseType(link) {
-  const { headers } = await axios.head(link)
+async function checkLinkResponseType(url, axiosOptions = {}) {
+  const { headers } = await axios({
+    method: 'head',
+    url,
+    ...axiosOptions,
+  })
 
   // content type is something other than html
   if (!headers?.['content-type']?.includes('text/html')) {
@@ -16,20 +20,24 @@ async function checkLinkResponseType(link) {
   return false
 }
 
-async function getLink(link) {
+async function getLink(url, axiosOptions = {}) {
   const validLink = new RegExp(/^(http|https):\/\/(?:www\.)?(mediafire)\.com\/[0-9a-z]+(\/.*)/gm)
 
-  if (!link.match(validLink)) throw new Error('Unknown link')
+  if (!url.match(validLink)) throw new Error('Unknown link')
 
   try {
     // make sure we are going to be handling html before requesting data
-    const type = await checkLinkResponseType(link)
+    const type = await checkLinkResponseType(url, axiosOptions)
 
     // TODO: return link based on content-type
     // right now we just assume anything besides text/html is a direct dl link
-    if (type) return link
+    if (type) return url
 
-    const { data } = await axios.get(link)
+    const { data } = await axios({
+      method: 'get',
+      url,
+      ...axiosOptions,
+    })
 
     const dom = new JSDOM(data)
     const downloadButton = dom.window.document.querySelector('#downloadButton')
@@ -40,7 +48,7 @@ async function getLink(link) {
   } catch (err) {
     if (err.response) {
       if (err.response.status === 404) {
-        throw new Error('The key you provided for file access was invalid.')
+        throw new Error('The key you provided for file access was invalid')
       }
 
       throw new Error(`Mediafire returned status ${err.response.status}`)
